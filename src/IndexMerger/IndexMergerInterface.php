cff5a7cb33739be0248a566da26bff651aa6e6e5
Arne Groskurth <arne.groskurth@gears9.de>
2018-03-04 12:11:23 +0100

    Doc

--- src/IndexMerger/IndexMergerInterface.php
+++ src/IndexMerger/IndexMergerInterface.php
@@ -8,6 +8,9 @@ use Archivr\Index;
 interface IndexMergerInterface
 {
     /**
+     * Injects the conflict handler to be used which is required if a conflict that cannot be resolved automatically is
+     * occurred.
+     *
      * @param ConflictHandlerInterface $conflictHandler
      * @return IndexMergerInterface
      */
@@ -19,6 +22,9 @@ interface IndexMergerInterface
     public function getConflictHandler(): ConflictHandlerInterface;
 
     /**
+     * Merges the given remote, local and lastLocal indices into a "merged" index representing the new state of the
+     * vault.
+     *
      * @param Index $remoteIndex
      * @param Index $localIndex
      * @param Index $lastLocalIndex
--- src/OperationCollectionBuilder/OperationCollectionBuilderInterface.php
+++ src/OperationCollectionBuilder/OperationCollectionBuilderInterface.php
@@ -7,5 +7,18 @@ use Archivr\OperationCollection;
 
 interface OperationCollectionBuilderInterface
 {
+    /**
+     * This method is called after the index merger has mergerd the local, lastLocal and remote indices into a "merged"
+     * index that represents the new state of the vault.
+     *
+     * Takes the local, remote and merged indices and has to return a ordered list of operations represented by
+     * implementations of the Archivr\Operation\OperationInterface that have to be executed for both the local and
+     * remote index equaling the merged index afterwards.
+     *
+     * @param Index $mergedIndex
+     * @param Index $localIndex
+     * @param Index|null $remoteIndex
+     * @return OperationCollection
+     */
     public function buildOperationCollection(Index $mergedIndex, Index $localIndex, Index $remoteIndex = null): OperationCollection;
 }
