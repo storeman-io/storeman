31e0124e660e750afa4670eecda4592418232cd6
Arne Groskurth <arne.groskurth@gears9.de>
2018-06-12 19:56:36 +0200

    Ensuring empty directory context

--- tests/Cli/Command/AbstractCommandTest.php
+++ tests/Cli/Command/AbstractCommandTest.php
@@ -3,13 +3,18 @@
 namespace Storeman\Test\Cli\Command;
 
 use PHPUnit\Framework\TestCase;
+use Storeman\Test\TemporaryPathGeneratorProviderTrait;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Tester\CommandTester;
 
 abstract class AbstractCommandTest extends TestCase
 {
+    use TemporaryPathGeneratorProviderTrait;
+
     public function testCallOutsideArchive(array $input = [])
     {
+        chdir($this->getTemporaryPathGenerator()->getTemporaryDirectory());
+
         $tester = new CommandTester($this->getCommand());
         $returnCode = $tester->execute($input);
 
