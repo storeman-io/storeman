767b2a3f90c48d89c7e906b9b9e6751ff7ca187d
Arne Groskurth <arne.groskurth@gears9.de>
2018-06-12 20:53:14 +0200

    Pulled command injection from Application instance

--- src/Cli/Application.php
+++ src/Cli/Application.php
@@ -2,12 +2,6 @@
 
 namespace Storeman\Cli;
 
-use Storeman\Cli\Command\DumpCommand;
-use Storeman\Cli\Command\InfoCommand;
-use Storeman\Cli\Command\InitCommand;
-use Storeman\Cli\Command\RestoreCommand;
-use Storeman\Cli\Command\SynchronizeCommand;
-
 class Application extends \Symfony\Component\Console\Application
 {
     const LOGO =  <<<TXT
@@ -22,16 +16,5 @@ TXT;
     public function getHelp()
     {
         return static::LOGO . parent::getHelp();
-    }
-
-    protected function getDefaultCommands()
-    {
-        return array_merge(parent::getDefaultCommands(), [
-            new DumpCommand(),
-            new InfoCommand(),
-            new InitCommand(),
-            new RestoreCommand(),
-            new SynchronizeCommand(),
-        ]);
     }
 }
