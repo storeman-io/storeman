e610ee00332ac9025b4dd37a43096d6b611f973b
Arne Groskurth <arne.groskurth@gears9.de>
2018-06-20 02:43:29 +0200

    Added script for quick hashing performance measurements

---
+++ doc/encryptionPerformanceTest.php
@@ -0,0 +1,31 @@
+<?php
+
+if ($argc < 3) {
+
+    exit("Usage: php {$argv[0]} [file] [algorithm] ([algorithm...])\n");
+}
+
+$fileHandle = fopen($argv[1], 'rb') or exit("fopen() failed for '{$argv[1]}'\n");
+
+$contexts = [];
+foreach (array_slice($argv, 2) as $algorithm)
+{
+    $contexts[$algorithm] = hash_init($algorithm);
+}
+
+while(!feof($fileHandle))
+{
+    $buffer = fread($fileHandle, 65536);
+
+    array_walk($contexts, function($context) use ($buffer) {
+
+        hash_update($context, $buffer);
+    });
+}
+
+array_walk($contexts, function(&$context, string $algorithm) {
+
+    print sprintf("{$algorithm}: %s\n", hash_final($context));
+});
+
+fclose($fileHandle);
